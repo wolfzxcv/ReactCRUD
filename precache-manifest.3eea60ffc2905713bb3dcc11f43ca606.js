d6360f58b6b43e6751dc1930a753e79abe7763de
Nien <wolfzxcv@gmail.com>
2019-10-07 11:03:41 +0200

    Updates

---
+++ precache-manifest.3eea60ffc2905713bb3dcc11f43ca606.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95bbaa720c8e3b850ec8c8242c507510",
+    "url": "/ReactCRUD/index.html"
+  },
+  {
+    "revision": "88d0530670ff14c429c9",
+    "url": "/ReactCRUD/static/css/main.76efb973.chunk.css"
+  },
+  {
+    "revision": "79646891ae9fc0e4dab7",
+    "url": "/ReactCRUD/static/js/2.dc6086f7.chunk.js"
+  },
+  {
+    "revision": "88d0530670ff14c429c9",
+    "url": "/ReactCRUD/static/js/main.23eaf966.chunk.js"
+  },
+  {
+    "revision": "fb2100e68fd4a1a634bf",
+    "url": "/ReactCRUD/static/js/runtime-main.b32ebc6c.js"
+  }
+]);
